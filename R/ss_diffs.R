88ff5123c15c2f764f17f05e73141f47f2f23a0c
Sollano Rabelo Braga <sollanorb@gmail.com>
2021-10-01 14:10:59 -0300

    fix diferent areas for non grouped data

--- R/ss_diffs.R
+++ R/ss_diffs.R
@@ -132,6 +132,12 @@ ss_diffs <- function(df, Yi, plot_area, total_area, m3ha=FALSE, age=NA, .groups=
   # Se .groups nao for fornecido, criar objeto que dplyr::group_by ignora, sem causar erro
   if(missing(.groups)||any(is.null(.groups))||any(is.na(.groups))||any(.groups==F)||any(.groups=="") ){
     .groups_syms <- character()
+    
+  # area total quando se tem areas separadas
+    if(length(unique(df[[total_area]]))>1 ){
+      df[[total_area]] = sum(unique(df[[total_area]]))
+      }  
+    
     # Se groups for fornecido verificar se todos os nomes de variaveis fornecidos existem no dado  
   }else if(!is.character(.groups)){
     stop(".groups must be a character", call.=F)
